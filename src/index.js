fad648c9389fd4838fe660bee3e965b51ee0eac3
aguldstrand <golfin85@gmail.com>
2016-12-30 13:08:33 +0100

    gulp puginify

--- src/index.js
+++ src/index.js
@@ -1,34 +1,60 @@
 var { generateMarkdown } = require('./ts/generator/markdown')
 var { generateSass } = require('./ts/generator/sass')
+var File = require('vinyl')
 var formatter = require('./ts/formatter')
 var fs = require('fs')
 var grammar = require('./grammar')
 var nearley = require('nearley')
 var nodeSass = require('node-sass');
+var path = require('path')
+var through2 = require('through2')
+
 
 var parser = new nearley.Parser(grammar.ParserRules, grammar.ParserStart)
 
-fs.readFile("src/styles/main.scss", 'utf8', (err, data) => {
+module.exports = function bemsass() {
 
-    parser.feed(data)
+    return through2.obj(function (file, enc, next) {
 
-    fs.writeFile("dist/schema.json", JSON.stringify(parser.results[0], null, 4), (err) => { })
+        const bemsass = file.contents.toString('utf8')
+        const base = path.join(file.base, '..')
 
-    const ast = parser.results[0]
-    const dom = formatter.root(ast)
-    fs.writeFile("dist/main.json", JSON.stringify(dom, null, 4), (err) => { })
+        parser.feed(data)
 
-    const sass = generateSass(dom)
-    fs.writeFile("dist/main.scss", sass, (err) => { })
+        fs.writeFile("dist/schema.json", JSON.stringify(parser.results[0], null, 4), (err) => { })
 
-    const markdown = generateMarkdown(dom)
-    fs.writeFile("dist/main.md", markdown, (err) => { })
+        const ast = parser.results[0]
+        const dom = formatter.root(ast)
+        this.push(new File({
+            base: base,
+            path: path.join(base, 'main.json'),
+            contents: new Buffer(JSON.stringify(dom, null, 4))
+        }))
 
-    nodeSass.render({
-        data: sass
-    }, function (err, result) {
-        fs.writeFile("dist/main.css", result.css, (err) => { })
-    });
+        const sass = generateSass(dom)
+        this.push(new File({
+            base: base,
+            path: path.join(base, 'main.scss'),
+            contents: new Buffer(sass)
+        }))
 
+        const markdown = generateMarkdown(dom)
+        this.push(new File({
+            base: base,
+            path: path.join(base, 'main.md'),
+            contents: new Buffer(markdown)
+        }))
 
-})
+        const sassResult = nodeSass.renderSync({
+            data: sass
+        })
+
+        this.push(new File({
+            base: base,
+            path: path.join(base, 'main.css'),
+            contents: new Buffer(sassResult.css)
+        }))
+
+    })
+
+}
